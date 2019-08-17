dc17e76c2ec2a10724ed911d1c6cfbf223c48cd2
HarishSivakumaran <s.harish740@gmail.com>
2019-08-17 19:38:03 +0530

    cacad

--- AVLTree/src/AVLTree.java
+++ AVLTree/src/AVLTree.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class AVLTree {
 
-    private Node root;
+    private static Node root;
 
     private static class Node {
         int data;
@@ -22,12 +22,12 @@ public class AVLTree {
             return node;
         }
         if (node.data > data) {
-            if(node.leftChild!=null)
-            return find(data, node.leftChild);
+            if (node.leftChild != null)
+                return find(data, node.leftChild);
             return node;
 
         } else if (data > node.data) {
-            if(node.rightChild!=null)
+            if (node.rightChild != null)
                 return find(data, node.rightChild);
             return node;
         }
@@ -35,44 +35,109 @@ public class AVLTree {
         return node;
     }
 
-    public static Node next(int data,Node node){
-        node = find(data,node);
+    public static Node findParent(int data, Node node) {
 
-        if(node.rightChild!=null){
+        if ((node.rightChild.data == data) || (node.leftChild.data == data)) {
+            return node;
+        }
+        if (node.data > data) {
+            if (node.leftChild != null)
+                return find(data, node.leftChild);
+            return node;
+
+        } else if (data > node.data) {
+            if (node.rightChild != null)
+                return find(data, node.rightChild);
+            return node;
+        }
+
+        return node;
+    }
+
+    public static Node next(int data, Node node) {
+
+        node = find(data, node);
+
+        if (node.rightChild != null) {
             return leftDescendant(node.rightChild);
-        }else{
+        } else {
             return rightAncestor(node.leftChild);
         }
     }
 
-    public static Node leftDescendant(Node node){
-        if(node.leftChild == null)
+    public static Node leftDescendant(Node node) {
+
+        if (node.leftChild == null)
             return node;
         return leftDescendant(node.leftChild);
     }
 
-    public static Node rightAncestor(Node node){
-        if(node.parent.data > node.data){
+    public static Node rightAncestor(Node node) {
+
+        if (node.parent.data > node.data) {
             return node.parent;
         }
-          if(node.parent != null)
-        return rightAncestor(node.parent);
+        if (node.parent != null)
+            return rightAncestor(node.parent);
 
         return node;
 
     }
 
-    public static ArrayList<Node> rangeOfSearch(int data,Node node,int endData){
-        node = find(data,node);
-        ArrayList<Node> arrayList= new ArrayList<>();
-        while(node.data <= endData){
-            if(node.data>= data)
+    public static ArrayList<Node> rangeOfSearch(int data, Node node, int endData) {
+
+        node = find(data, node);
+        ArrayList<Node> arrayList = new ArrayList<>();
+        while (node.data <= endData) {
+            if (node.data >= data)
                 arrayList.add(node);
-            node = next(data,node);
+            node = next(data, node);
+        }
+        return arrayList;
+    }
+
+    private static void insertion(Node node) {
+
+        if (root == null) {
+            root = node;
+            return;
+        }
+        Node nodeToInsert = find(node.data, node);
+        if (node.data < nodeToInsert.data) {
+            nodeToInsert.leftChild = node;
+            node.parent = nodeToInsert;
+        } else {
+            nodeToInsert.rightChild = node;
+            node.parent = nodeToInsert;
+
         }
 
-        return arrayList;
+    }
 
+
+    public static Node deletion(int data, Node root) {
+
+        if (root == null)
+            return root;
+
+        if (root.data < data) {
+            root.rightChild = deletion(data, root.rightChild);
+        } else if (data < root.data) {
+            root.leftChild = deletion(data, root.leftChild);
+        } else {
+            if (root.rightChild == null) {
+                return root.leftChild;
+            } else if (root.leftChild == null) {
+                return root.rightChild;
+            }
+
+            root.data= next(root.data,root).data;
+
+         root.rightChild=   deletion(root.data,root.rightChild);
+
+        }
+
+        return  root;
     }
 
     public static void main(String[] args) {
